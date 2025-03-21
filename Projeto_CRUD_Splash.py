7702cfc30c9976f4b9dd38bba1166cbc9600d4b2
Leonardo-F-Santana <discipuloleonardo@gmail.com>
2025-03-20 21:00:34 -0300

    Icone de janela Adicionado

--- Projeto_CRUD_Splash.py
+++ Projeto_CRUD_Splash.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from PIL import Image, ImageTk
 
 root = Tk()
 
@@ -13,4 +14,10 @@ class Application():
         self.root.geometry('750x580')
         self.root.resizable(True, True) #Responsividade da tela (Altura x Largura)
         self.root.minsize(width= 300, height= 200)
+
+        icon_image = Image.open('logo_splash.png')  # Substitua pelo caminho do arquivo .png
+        icon_photo = ImageTk.PhotoImage(icon_image)
+        self.root.iconphoto(True, icon_photo)
+
 Application()
+

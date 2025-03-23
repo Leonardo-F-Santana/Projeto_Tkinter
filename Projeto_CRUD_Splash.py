9f228ddf8067e9d978027507ae4cfbff78cde008
Leonardo-F-Santana <discipuloleonardo@gmail.com>
2025-03-22 21:58:49 -0300

    InserÇão dos dois frames no root

--- Projeto_CRUD_Splash.py
+++ Projeto_CRUD_Splash.py
@@ -1,14 +1,16 @@
 from tkinter import *
 from PIL import Image, ImageTk
 
-root = Tk()
+root = Tk() #janela principal
 
 class Application():
     def __init__(self):
-        self.root = root
-        self.tela()
-        root.mainloop()
-    def tela(self):
+        self.root = root #vinculo da classe com objeto
+        self.tela() #metodo que configura a tela
+        self.frames_de_tela()
+        root.mainloop() #outro metodo que mantem a tela ativa independente de interação
+
+    def tela(self): #essa função configura aparencia e comportamento da tela 
         self.root.title('Cadastro de Moradores')
         self.root.configure(background= '#ffa900')
         self.root.geometry('750x580')
@@ -18,6 +20,14 @@ class Application():
         icon_image = Image.open('logo_splash.png')  # Substitua pelo caminho do arquivo .png
         icon_photo = ImageTk.PhotoImage(icon_image)
         self.root.iconphoto(True, icon_photo)
+
+    def frames_de_tela(self): #parametros da moldura de cima
+        self.frame_1 = Frame(self.root, bd= 4, bg="#FFF", highlightbackground="#C0C0C0", highlightthickness=3) # borda, bg do frame, highlightbg cor do fundo e ...thickness largura da borda
+        self.frame_1.place(relx=0.02, rely=0.02, relwidth=0.96, relheight=0.46) #relx = margem lado (entre 0 e 1 = 100%), rely = margem cima (entre 0 e 1), relwidt = largura do frame, relheight = altura do frame
+
+        self.frame_2 = Frame(self.root, bd= 4, bg="#FFF", highlightbackground="#C0C0C0", highlightthickness=3) # borda, bg do frame, highlightbg cor do fundo e ...thickness largura da borda
+        self.frame_2.place(relx=0.02, rely=0.5, relwidth=0.96, relheight=0.46)
+    
 
 Application()
 
